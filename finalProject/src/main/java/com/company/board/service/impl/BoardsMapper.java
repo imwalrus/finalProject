39d8ecd51ab86345f963cf2a84216d243baef22d
im_walrus <im_walrus@naver.com>
2021-03-26 17:36:32 +0900

    1

--- finalProject/src/main/java/com/company/board/service/impl/BoardsMapper.java
+++
@@ -1,9 +0,0 @@
-package com.company.board.service.impl;
-
-import com.company.board.service.BoardsVO;
-
-public interface BoardsMapper {
-	public int insertBoards(BoardsVO vo);
-	
-	public BoardsVO getBoards(BoardsVO vo);
-}
--- finalProject/src/main/java/com/company/temp/TestVO.java
+++
@@ -1,9 +0,0 @@
-package com.company.temp;
-
-import lombok.Data;
-
-@Data
-public class TestVO {
-	private String firstName;
-	private String salary;	
-}
