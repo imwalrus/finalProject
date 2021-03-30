35451c514897e486c1ba155d233121d407401180
hyewon <fray7776@gmail.com>
2021-03-30 18:12:48 +0900

    농촌속으로-등록(파일 정규식표현으로 업로드 찾는중..)

---
+++ finalProject1/src/main/java/co/finalproject/farm/app/intoTheFarm/controller/IntoTheFarmController.java
@@ -0,0 +1,69 @@
+package co.finalproject.farm.app.intoTheFarm.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.google.gson.JsonObject;
+
+import co.finalproject.farm.app.intoTheFarm.service.IntoTheFarmVO;
+import co.finalproject.farm.app.intoTheFarm.service.impl.IntoTheFarmMapper;
+
+@Controller
+public class IntoTheFarmController {
+	@Autowired IntoTheFarmMapper intoTheFarmMapper;
+	
+	//전체조회
+	@RequestMapping("/getFarmList")
+	public String getFarmList(IntoTheFarmVO vo, Model model) {
+		model.addAttribute("list", intoTheFarmMapper.getFarmList(vo));
+		System.out.println("list");
+		return "intoTheFarm";
+	}
+	
+	//단건조회
+	@RequestMapping("/getSearchFarm")
+	public String getSearchFarm(IntoTheFarmVO vo, Model model) {
+		model.addAttribute("list",intoTheFarmMapper.getSearchFarm(vo));
+		return "";
+	}
+	//등록
+	@GetMapping("/insertFarm") //등록 페이지
+	public String insertFarm(IntoTheFarmVO vo,Model model) {
+		return "insertIntoFarm";
+	}
+	
+	@PostMapping("/insertFarm") //등록
+	public String insertFarmProc(IntoTheFarmVO vo) {
+		intoTheFarmMapper.insertFarm(vo);
+		return "redirect:/getFarmList";
+	}
+	
+	//수정
+	@RequestMapping("/updateFarm")
+	public String updateFarm(IntoTheFarmVO vo) {
+		intoTheFarmMapper.updateFarm(vo);
+		return "";
+	}
+	//삭제
+	@RequestMapping("/deleteFarm")
+	public String deleteFarm(IntoTheFarmVO vo) {
+		intoTheFarmMapper.updateFarm(vo);
+		return "";
+	}
+}
---
+++ finalProject1/src/main/java/co/finalproject/farm/app/intoTheFarm/service/IntoTheFarmVO.java
@@ -0,0 +1,22 @@
+package co.finalproject.farm.app.intoTheFarm.service;
+
+import java.sql.Date;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import lombok.Data;
+
+@Data
+public class IntoTheFarmVO {
+	private String into_no;
+	private String into_title;
+	private String into_phone;
+	private String into_product;
+	private Date into_date;
+	private String into_entry;
+	private String into_info;
+	private String user_id;
+	private String into_city;
+	private String into_filename;
+	private MultipartFile[] uploadFile;
+}
