a333b7ee7b9fd8c6d96fa5a5ea32de617c85d0c6
im_walrus <im_walrus@naver.com>
2021-04-08 19:09:38 +0900

    농산물판매 마무리중

--- finalProject/src/main/java/co/finalproject/farm/app/shop/service/impl/ShopMapper.java
+++ finalProject/src/main/java/co/finalproject/farm/app/shop/service/impl/ShopMapper.java
@@ -21,7 +21,7 @@ public interface ShopMapper {
 
 	// 개별 상품 페이지
 	public ShopVO getProduct(ShopVO vo);
-	
+
 	// 상품 관리 리스트
 	public List<ShopVO> getListProd(ShopVO vo);
 
@@ -33,29 +33,29 @@ public interface ShopMapper {
 
 	// 상품 삭제
 	public int deleteProduct(ShopVO vo);
-	
+
 	// 장바구니 페이지
 	public List<CartVO> getCart(CartVO vo);
-	
+
 	// 장바구니 등록
 	public void insertCart(CartVO vo);
-	
+
 	// 장바구니 수량 변경
 	public int updateCart(CartVO vo);
-	
+
 	// 장바구니 삭제
 	public int deleteCart(CartVO vo);
 
 	// 장바구니 비우기
 	public int deleteAllCart(CartVO vo);
-	
+
 	// 동일한 상품 체크
 	public int cartCheck(CartVO vo);
-	
+
 	// 주문 등록
 	public void insertOrder(OrderVO vo);
-	
+
 	// 주문 상세등록
 	public void insertOrderlist(OrderVO vo);
-	
+
 }
